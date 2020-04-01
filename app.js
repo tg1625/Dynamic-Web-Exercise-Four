febf36165dcc1a79c0d1e8bbf182b8919f5539e1
tg1625 <35944491+tg1625@users.noreply.github.com>
2020-04-01 09:11:51 -0400

    Add heroku

---
+++ app.js
@@ -0,0 +1,10 @@
+//import express
+const express = require('express')
+//initiate express to app
+const app = express()
+//set pot
+const port = 4000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
