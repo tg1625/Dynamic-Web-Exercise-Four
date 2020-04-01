94dfdbb3a62bf541e6731c68107cfc2c2e7ea63c
tg1625 <35944491+tg1625@users.noreply.github.com>
2020-04-01 09:35:47 -0400

    fix package

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 //initiate express to app
 const app = express()
-//set pot
+//set port
 const port = 4000
 
 app.get('/', (req, res) => res.send('Hello World!'))
